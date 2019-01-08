d5f17b3571c45de65419cd436c5a724a980cf006
Issac Chan <issaczhaoming.chan@my.jcu.edu.au>
2019-01-08 10:56:46 +0800

    add electricity_bill_estimator code

--- Practical 1/electricity_bill_estimator.py
+++ Practical 1/electricity_bill_estimator.py
@@ -1,11 +1,19 @@
+TARIFF_11 = 0.244618
+TARIFF_31 = 0.136928
+
 def main():
-    electricityUsed = int(input("Please enter the amount of electricity used in kWh:"))
-    daysUsed = int(input("Please enter the amount of electricity used days:"))
-    chargeRate = int(input("Please enter the price in cents:"))
+    electricityUsed = int(input("Which tariff? 11 or 31: "))
+    daysUsed = int(input("Please enter the amount of electricity used days: "))
+    chargeRate = int(input("Please enter the price in cents: "))
+
+    if electricityUsed == 11:
+        electricityUsed = TARIFF_11
+    elif electricityUsed == 31:
+        electricityUsed = TARIFF_31
 
     totalElectricity = electricityUsed * daysUsed
     totalBill = (totalElectricity * chargeRate)/100
 
-    print("Total billable amount for electricity use:", totalBill)
+    print("Total billable amount for electricity use:", format(totalBill, '.2f'))
 
 main()
